b8572a1b30b32d3a0938dba061b2425be968d8d3
Kot <alphakot@openmailbox.org>
2018-01-16 16:29:54 -0800

    ugh please work

--- cogs/fun.py
+++ cogs/fun.py
@@ -21,7 +21,7 @@ class Fun:
     async def osu(self, ctx, user):
         async with ClientSession() as session:
             async with session.get(f'https://osu.ppy.sh/api/get_user?k={self.config.osu}&u={user}') as response:
-                response = await response.json.to_dict()
+                response = await response.json
         await ctx.send(response)
 
     @commands.command()
